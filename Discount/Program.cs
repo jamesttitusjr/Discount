fa4e4b5490b7b5ff43ae6d97c763b05b15e978e2
James Titus <jamesttitusjr@outlook.com>
2023-01-30 18:27:50 -0500

    Add project files.

---
+++ Discount/Program.cs
@@ -0,0 +1,34 @@
+﻿
+
+
+namespace Discount
+{
+    internal class Customer
+    {
+    public int CustomerID { get; set; }
+    public string Name { get; set; }
+
+    //public decimal Discount { get; set; }
+
+    private decimal _discount;
+
+    public decimal Discount
+        {
+        get { return _discount; }
+        set
+            {
+            if (value >= 0 && value <= 1)
+                {
+                _discount = value;
+                }
+            else
+                {
+                throw new ArgumentOutOfRangeException("Discount must 0 to 1 (0% to 100%)");
+                }
+            }
+        }
+
+
+
+    }
+}
